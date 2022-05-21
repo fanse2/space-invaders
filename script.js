09d14a4efb3ba6eae7ebd9e0f869818e8164a2e9
fanse2 <fanse2@gmail.com>
2022-05-21 09:58:05 -0700

    2nd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const grid = document.querySelector('#grid')
 // const sprites = document.querySelector('#sprites')
-//16*16
+//16*16 -> 32*32
 const e1 = document.querySelector('#e1')
 const e2 = document.querySelector('#e2')
 const e3 = document.querySelector('#e3')
@@ -13,39 +13,32 @@ const ex3 = document.querySelector('#ex3')
 const ex4 = document.querySelector('#ex4')
 const ex5 = document.querySelector('#ex5')
 const jet = document.querySelector('#jet')
+const backups = document.querySelector('.part12')
 
-let ctx = grid.getContext('2d')
+const gaWidth = 480
+const gaHeight = 540
+const sWidth = 32
+
+const ctx = grid.getContext('2d')
 
 const invaders = []
 
-
-
-function pickImg() {
-    // img 14px * 14px
-    // 28 *20x, 42 *20x
-    
-    ctx.drawImage(jet, 100,100,32,32)
-    ctx.drawImage(e1, 100,140,32,32)
-    ctx.drawImage(e2, 100,180,32,32)
-    ctx.drawImage(e3, 100,220,32,32)
-
-
-
-}
-
 class Invader {
-    constructor(x,y,type=e1) {
+    constructor(x,y,type,ctx) {
         this.x = x
         this.y = y
         this.type = type
+        this.ctx = ctx
     }
 
     update() {
 
     }
 
-    draw(ctx) {
-        ctx.draw(this.type, this.x, this.y, 32, 32)
+    draw() {
+
+        ctx.drawImage(this.type, this.x, this.y, sWidth, sWidth)
+        // console.log(this.type, this.x, this.y, sWidth, sWidth)
     }
 }
 
@@ -53,9 +46,45 @@ class Jet {
 
 }
 
+function setting() {
+    // img 14px * 14p   
+    // adding invaders
+    for(let i=0; i<10; i++) {
+        invaders.push(new Invader(10 + i*(sWidth+5),10,e1,ctx))
+        invaders.push(new Invader(10 + i*(sWidth+5),10+sWidth+5,e2,ctx))
+        invaders.push(new Invader(10 + i*(sWidth+5),10+(sWidth+5)*2,e3,ctx))
+        invaders.push(new Invader(10 + i*(sWidth+5),10+(sWidth+5)*3,e4,ctx))
+        
+    }
+    
+    ctx.drawImage(jet, gaWidth/2 -sWidth/2 ,500,sWidth,sWidth)
+
+    invaders.forEach(v=>{
+        v.draw()
+    })
+    // ctx.drawImage(e1, 100,140,sWidth,sWidth)
+    // ctx.drawImage(e2, 100,180,sWidth,sWidth)
+    // ctx.drawImage(e3, 100,220,sWidth,sWidth)
+
+}
+
 function start() {
-    pickImg()
+
+    setting()
+    drawBackups(2)
 }
 
 
 
+function drawBackups(count) {
+    
+    backups.innerHTML=''
+    for(let i=0; i<count; i++){
+        let img = document.createElement('img')
+        img.src = jet.src
+        img.width = 26
+        img.height = 26
+        setTimeout(()=>backups.appendChild(img),300*(i+1))
+        
+    }
+}
