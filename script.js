616a29282473f0bd255336a7c41b66e9f1d9c44e
June Choi <fanse2@gmail.com>
2022-05-25 17:00:50 -0700

    enemybullet

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const ex5 = document.querySelector('#ex5')
 const jet = document.querySelector('#jet')
 const backups = document.querySelector('.part12')
 const scoreBD = document.querySelector('.part4')
+const countBD = document.querySelector('.part11')
+const stageBD = document.querySelector('.part6')
+
 
 const gaWidth = 480
 const gaHeight = 540
@@ -22,10 +25,13 @@ const sWidth = 32
 let score = 0
 let moveDirection = 1
 let jetSpeed = 10
+let stage = 1
+let shooterCnt = 3
 
 const ctx = grid.getContext('2d')
 
 let gameInt
+let bulletInt
 
 class Invader {
     constructor(x,y,type,ctx) {
@@ -48,11 +54,11 @@ class Invader {
     }
 
     chkCollision(x,y) {
-        if(x>this.x && x<this.x+sWidth && y>this.y && y<this.y+sWidth && this.condition) return true
+        if(x>this.x && x<this.x+sWidth && y>this.y && y<this.y+sWidth) return true
         return false
     }
 }
-
+// Jet = Shooter
 class Jet {
     constructor(x,y,ctx) {
         this.x = x
@@ -61,16 +67,24 @@ class Jet {
         this.speed = 0
     }
 
-    // update(dx) {
-    //     this.x += dx
-    // }
-
     update() {
         this.x += this.speed
     }
 
     draw() {
         ctx.drawImage(jet, this.x ,this.y ,sWidth,sWidth)
+    }
+
+    chkCollision(x,y) {
+
+        console.log(x,'-',y,':',this.x,'-',this.y)
+        if(x>this.x && x<this.x+sWidth && y>this.y && y<this.y+sWidth && this.condition) return true
+        return false
+    }
+
+    initPosition() {
+        this.x = gaWidth/2 -sWidth/2 
+        this.y = 500
     }
 
 }
@@ -87,7 +101,7 @@ class Bullet {
     }
 
     draw() {
-        ctx.strokeStyle = 'red'
+        ctx.strokeStyle = 'blue'
         ctx.lineWidth = 2
 
         ctx.beginPath()
@@ -96,48 +110,106 @@ class Bullet {
         ctx.stroke()
     }
 }
-const invaders = []
-let shooter = new Jet(gaWidth/2 -sWidth/2 ,500, ctx)
-const bullets = []
 
-function setting() {
+class EnemyBullet extends Bullet {
+    draw() {
+        ctx.strokeStyle = 'red'
+        ctx.lineWidth = 2
+
+        ctx.beginPath()
+        ctx.moveTo(this.x, this.y)
+        ctx.lineTo(this.x, this.y-10)
+        ctx.stroke()
+    }
+}
+
+let invaders = []
+let shooter = new Jet(gaWidth/2 -sWidth/2 ,500, ctx)
+let bullets = []
+let eBullets = []
+
+function setupStage(stage=1) {
     // img 14px * 14p   
-    // adding invaders
+    // adding invaders 8*4=32 invaders
     for(let i=0; i<8; i++) {
         invaders.push(new Invader(10 + i*(sWidth+15),20,e1,ctx))
         invaders.push(new Invader(10 + i*(sWidth+15),20+sWidth+5,e2,ctx))
         invaders.push(new Invader(10 + i*(sWidth+15),20+(sWidth+5)*2,e3,ctx))
         invaders.push(new Invader(10 + i*(sWidth+15),20+(sWidth+5)*3,e4,ctx))
-        
     }
 
     invaders.forEach(v=>{
         v.draw()
     })
 
+    shooter.initPosition()
     shooter.draw()
+    stageBD.innerHTML = stage
+    drawBackups(shooterCnt-1)
+    gameInt = setInterval(gameLoop, 50)
+    bulletInt = setInterval(bulletLoop,1000)
+}
 
+function finishStage() {
+    clearInterval(gameInt)
+    clearInterval(bulletInt)
+    bullets = []
+    invaders = []
+    alert('You bit them all!! and Scored ' + score)
+    
+    setupStage(++stage)
+
+
+}
+
+function failStage() {
+    clearInterval(gameInt)
+    clearInterval(bulletInt)
+    bullets = []
+    invaders = []
+    shooterCnt--
+    alert('you failed!')
+    if(shooterCnt>0) {
+        setupStage(stage) 
+    } else {
+        countBD.innerHTML = 0
+        alert('game over!!')
+    }
+}
+
+function clearStage() {
+
+}
+
+function setupLoop() {
+
+}
+
+function bulletLoop() {
+    let cnt = invaders.length
+    let shootingInvader = invaders[Math.floor(Math.random()*cnt)]
+    eBullets.push(new EnemyBullet(shootingInvader.x + sWidth / 2 + 1  , shootingInvader.y, ctx))
+    //console.log('ebullets: ',eBullets.length)
 }
 
 function gameLoop() {
     let dy = 0
 
+    // if destroyed them all finish stage
     if(invaders.length==0) {
-        clearInterval(gameInt)
-        alert('You bit them all!! and Scored ' + score)
-
+        finishStage()
     }
-
+    // invaders turn over
     if(invaders.some(v=>v.x < 10 || v.x > gaWidth - sWidth - 10)) {
         dy = 10
         moveDirection *= -1
     }
-
+    // shooter move range limit
     if((shooter.x > 10 && shooter.speed < 0) || (shooter.x < gaWidth - sWidth - 10 && shooter.speed > 0) ) {
         shooter.update()
     } 
 
-    //bullets move
+    // bullets move
     bullets.forEach(v=>v.update(-20))
     bullets.forEach((v,i,a)=>{
         if(v.y < 0) {
@@ -145,9 +217,9 @@ function gameLoop() {
         }
         invaders.forEach((vv,ii,aa)=>{
             if(vv.chkCollision(v.x,v.y)){
-                a.splice(i,1)       //bullit removal
+                a.splice(i,1)       //bullet removal
 
-                //explode & invader removal
+                //explode & invader removal animation
                 vv.type=ex1
                 setTimeout(()=>vv.type=ex2,10)
                 setTimeout(()=>vv.type=ex3,15)
@@ -163,10 +235,20 @@ function gameLoop() {
         })
         
     })
-    
-    //invaders move
+    // enemy bullets move
+    eBullets.forEach(v=>v.update(10+stage/2))
+    eBullets.forEach((v,i,a)=>{
+        if(v.y > 500 + sWidth) {
+            a.splice(i,1)       //bullet removal
+        }
+        if(shooter.chkCollision(v.x,v.y)) {
+            alert('got shot!')
+            failStage()
+        }
+    })
+    // invaders move, speed up .2px every stage 
     invaders.forEach(v=>{
-        v.update(2 * moveDirection, dy)
+        v.update((2 + stage*.2) * moveDirection, dy)
     })
 
     ctx.clearRect(0, 0, gaWidth, gaHeight)
@@ -178,22 +260,27 @@ function gameLoop() {
     shooter.draw()
 
     bullets.forEach(v=>v.draw())
+    eBullets.forEach(v=>v.draw())
 
     scoreBD.innerHTML = score
+    countBD.innerHTML = shooterCnt
 
-    console.log('count :',bullets.length)
+    // invaders reached end
+    if(invaders.some(v=>v.y > 350)) {
+        failStage()
+    }
+
+    // console.log('count :',eBullets.length)
 }
 
 function start() {
 
-    setting()
-    drawBackups(2)
-    gameInt = setInterval(gameLoop, 50)
+    setupStage()
 
 }
 
 function moveShooter(e) {
-    console.log(e.key+" / "+e.code)
+    // console.log(e.key+" / "+e.code)
     switch(e.code) {
         case 'ArrowLeft':
             shooter.speed = -10
@@ -208,7 +295,6 @@ function stopShooter(e) {
         shooter.speed = 0
     }
 }
-
 function fire(e) {
     if(e.code=='Space')  bullets.push(new Bullet(shooter.x + sWidth / 2 + 1  , 500, ctx))
 }
@@ -216,8 +302,6 @@ function fire(e) {
 document.addEventListener('keydown', moveShooter)
 document.addEventListener('keypress', fire)
 document.addEventListener('keyup', stopShooter)
-
-
 
 function drawBackups(count) {
     
